c3eb6334cfbf3ecf97dad32c593a2ba2396f2549
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-03-18 09:23:30 +0100

    Implemented the contracts for managing repositories

--- AfricanKitchen/Services/Services.ProductAPI/RepositoriesManager/ProductRepositoryStore/ProductRepository.cs
+++ AfricanKitchen/Services/Services.ProductAPI/RepositoriesManager/ProductRepositoryStore/ProductRepository.cs
@@ -1,6 +1,58 @@
-﻿namespace Services.ProductAPI.RepositoriesManager.ProductRepositoryStore
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Services.ProductAPI.Contracts.IRepositoryManager.ProductRepositoryStore;
+using Services.ProductAPI.DbContexts;
+using Services.ProductAPI.Models;
+
+namespace Services.ProductAPI.RepositoriesManager.ProductRepositoryStore
 {
-    public class ProductRepository
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
     {
+        public ProductRepository(ApplicationDbContext repositoryContext) : base(repositoryContext)
+        {
+
+        }
+        public void CreateUpdateProduct([FromBody] Product product)
+        {
+            if (product.ProductId > 0)
+            {
+                Update(product);
+            }
+            else
+            {
+                Create(product);
+            }
+        }
+
+        public async Task<bool> DeleteProduct(int productId)
+        {
+            try
+            {
+                var productToDelete = await FindByCondition(p => p.ProductId == productId).SingleOrDefaultAsync();
+                if (productToDelete == null)
+                {
+                    return false;
+                }
+                Delete(productToDelete);
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public async Task<Product> GetProductById(int productId) =>
+            await FindByCondition(p => p.ProductId == productId)
+            .SingleOrDefaultAsync();
+
+
+
+        public async Task<IEnumerable<Product>> GetProducts() =>
+         await FindAll()
+         .OrderBy(p => p.ProductId)
+         .ToListAsync();
     }
 }
--- AfricanKitchen/Services/Services.ProductAPI/RepositoriesManager/RepositoryManager.cs
+++ AfricanKitchen/Services/Services.ProductAPI/RepositoriesManager/RepositoryManager.cs
@@ -1,6 +1,32 @@
-﻿namespace Services.ProductAPI.RepositoriesManager
+﻿using Services.ProductAPI.Contracts.IRepositoryManager;
+using Services.ProductAPI.Contracts.IRepositoryManager.ProductRepositoryStore;
+using Services.ProductAPI.DbContexts;
+using Services.ProductAPI.RepositoriesManager.ProductRepositoryStore;
+
+namespace Services.ProductAPI.RepositoriesManager
 {
-    public class RepositoryManager
+    public class RepositoryManager : IRepositoryManager
     {
+        private ApplicationDbContext _repositoryContext;
+        private IProductRepository _productRepository;
+
+        public RepositoryManager(ApplicationDbContext repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+        }
+
+        public IProductRepository Product
+        {
+            get
+            {
+                if( _productRepository == null)
+                {
+                    _productRepository =  new ProductRepository(_repositoryContext)
+                }
+                return _productRepository;
+            }
+        }
+
+        public Task SaveAsync() => _repositoryContext.SaveChangesAsync();
     }
 }
