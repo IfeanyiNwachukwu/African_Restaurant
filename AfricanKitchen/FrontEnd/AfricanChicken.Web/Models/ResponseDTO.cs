ce67c434f67b9dd400d5394879695e7fef92f485
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-03-18 10:57:25 +0100

    Added the required DTO's

---
+++ AfricanKitchen/FrontEnd/AfricanChicken.Web/Models/ResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace AfricanChicken.Web.Models
+{
+    public class ResponseDTO
+    {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = string.Empty;
+        public List<string> ErrorMessages { get; set; }
+    }
+}
