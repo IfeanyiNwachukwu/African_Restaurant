cb6d7e4ea3f4e31272ab26ca441750c9d4e35db5
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-03-18 09:33:05 +0100

    Added a General Response DTO to be used by every endpoint in the project

--- AfricanKitchen/Services/Services.ProductAPI/Helpers/ResponseDTO.cs
+++ AfricanKitchen/Services/Services.ProductAPI/Helpers/ResponseDTO.cs
@@ -2,5 +2,9 @@
 {
     public class ResponseDTO
     {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = string.Empty;
+        public List<string> ErrorMessages { get; set; }
     }
 }
