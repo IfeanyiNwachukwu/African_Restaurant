6cc46dacbeb04c63defceea3ff96618180c04dc8
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-03-18 15:58:41 +0100

    Added IdentityDbContext and ApplicationUser

---
+++ AfricanKitchen/Services/Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Services.Identity.Models;
+
+namespace Services.Identity.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ AfricanKitchen/Services/Services.Identity/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Services.Identity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
