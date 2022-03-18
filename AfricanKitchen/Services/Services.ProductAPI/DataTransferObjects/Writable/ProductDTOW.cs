49108e33971d57b3a2139fd9242fb5f4c3b7249a
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-03-18 06:05:31 +0100

    Added a Product Write DTO

--- AfricanKitchen/Services/Services.ProductAPI/DataTransferObjects/Writable/ProductDTOW.cs
+++ AfricanKitchen/Services/Services.ProductAPI/DataTransferObjects/Writable/ProductDTOW.cs
@@ -1,6 +1,15 @@
-﻿namespace Services.ProductAPI.DataTransferObjects.Writable
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Services.ProductAPI.DataTransferObjects.Writable
 {
     public class ProductDTOW
     {
+        [Required]
+        public string Name { get; set; }
+        [Range(1, 1000)]
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
