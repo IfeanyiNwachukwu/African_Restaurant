88dcd9ba7c7a7f8a19b8ca27507e3b79f2f0d744
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-04-20 14:13:35 +0100

    Updated Detais and Index razor pages

---
+++ AfricanKitchen/Services/Services.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Services.ShoppingCartAPI.DbContexts
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        }
+        //public DbSet<Product> Products { get; set; }
+    }
+}
