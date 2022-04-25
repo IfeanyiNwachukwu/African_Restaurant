2f13b9ae37576c9f567157ed3b3df2e30db7d247
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-04-25 15:06:35 +0100

    completed the controler code

---
+++ AfricanKitchen/Services/Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.CouponAPI.Contracts.CouponRepositoryStore;
+using Services.CouponAPI.Helpers;
+
+namespace Services.CouponAPI.Controllers
+{
+    [ApiController]
+    [Route("api/coupon")]
+    public class CouponController : Controller
+    {
+        private readonly ICouponRepository _couponRepository;
+        protected ResponseDTO _response;
+
+        public CouponController(ICouponRepository couponRepository)
+        {
+            _couponRepository = couponRepository;
+            _response = new ResponseDTO();
+        }
+        [HttpGet("{code}")]
+        public async Task<object> GetDiscountForCode(string code)
+        {
+            try
+            {
+                var coupon = await _couponRepository.GetCouponByCode(code);
+                _response.Result = coupon;
+               
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+                
+            }
+            return _response;
+        }
+    }
+}
