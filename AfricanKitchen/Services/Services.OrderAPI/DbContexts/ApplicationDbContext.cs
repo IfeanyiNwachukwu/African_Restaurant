ce29e02ea7c0b1abe0d8418fda92eec6d9d4660f
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-04-27 11:23:05 +0100

    Added models and DbContext classes

---
+++ AfricanKitchen/Services/Services.OrderAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Services.OrderAPI.Models;
+
+namespace Services.OrderAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+    }
+}
