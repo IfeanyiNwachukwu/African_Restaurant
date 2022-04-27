4c1a2214390b8cf2d401e592e79d82bb68dbbd44
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-04-27 16:30:20 +0100

    committing changes

---
+++ AfricanKitchen/Services/Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,73 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using Services.OrderAPI.Fulfilment.OrderRepositoryStore;
+using Services.OrderAPI.Messages;
+using Services.OrderAPI.Models;
+using System.Text;
+
+
+namespace Services.OrderAPI.Messaging
+{
+    public class AzureServiceBusConsumer
+    {
+        private readonly string serviceBusConnectionString;
+        private readonly string subscriptionName;
+        private readonly string checkoutMessageTopic;
+        private readonly OrderRepository _orderRepository;
+
+        private ServiceBusProcessor checkOutProcessor;
+
+        private readonly IConfiguration _configuration;
+
+        public AzureServiceBusConsumer(OrderRepository orderRepository,IConfiguration configuration)
+        {
+            _orderRepository = orderRepository;
+            _configuration = configuration;
+
+            serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString");
+            subscriptionName = _configuration.GetValue<string>("checkoutmessagetopic");
+            checkoutMessageTopic = _configuration.GetValue<string>("AfricanKitchenOrderSubscription");
+        }
+        private async Task OnCheckOutMessageReceived(ProcessMessageEventArgs args) 
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            CheckoutHeaderDTO checkoutHeaderDTO = JsonConvert.DeserializeObject<CheckoutHeaderDTO>(body);
+
+            OrderHeader orderHeader = new()
+            {
+                UserId = checkoutHeaderDTO.UserId,
+                FirstName = checkoutHeaderDTO.FirstName,
+                LastName = checkoutHeaderDTO.LastName,
+                OrderDetails = new List<OrderDetails>(),
+                CardNumber = checkoutHeaderDTO.CardNumber,
+                CouponCode = checkoutHeaderDTO.CouponCode,
+                CVV = checkoutHeaderDTO.CVV,
+                DiscountTotal = checkoutHeaderDTO.DiscountTotal,
+                Email = checkoutHeaderDTO.Email,
+                ExpiryMonthYear = checkoutHeaderDTO.ExpiryMonthYear,
+                OrderTime = DateTime.Now,
+                OrderTotal = checkoutHeaderDTO.OrderTotal,
+                PaymentStatus = false,
+                Phone = checkoutHeaderDTO.Phone,
+                PickupDateTime = checkoutHeaderDTO.PickupDateTime,
+            };
+            foreach(var detailList in checkoutHeaderDTO.CartDetails)
+            {
+                OrderDetails orderDetails = new()
+                {
+                    ProductId = detailList.ProductId,
+                    ProductName = detailList.Product.Name,
+                    Price = detailList.Product.Price,
+                    Count = detailList.Count
+                };
+                orderHeader.CartTotalItems += detailList.Count;
+                orderHeader.OrderDetails.Add(orderDetails);
+            }
+
+            await _orderRepository.AddOrder(orderHeader);
+           
+        }
+    }
+}
